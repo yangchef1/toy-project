f2d041b3b43a2f33acc213eb6f72dee71c606314
yang <hc1489@naver.com>
2023-05-12 15:01:13 +0900

    localhost5500 추가

--- config/settings.py
+++ config/settings.py
@@ -81,6 +81,8 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [ 
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
 ]
 
 ROOT_URLCONF = 'config.urls'
